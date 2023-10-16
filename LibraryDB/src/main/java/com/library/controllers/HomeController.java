8fe3f4230111a73511ed5220d1b9d4403b61857b
PatrykWarchocki <patryk.warchocki@gmail.com>
2023-10-16 10:10:32 +0200

    Controllers Setup

---
+++ LibraryDB/src/main/java/com/library/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.library.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+	@GetMapping("/home")
+	public String Home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "home";
+	}
+}
